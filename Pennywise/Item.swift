a2e9fbed7f3fa9c48c11a14237eabc700cb6621b
Arnav Varyani <arnavvaryani@gmail.com>
2025-04-08 14:53:04 -0400

    Initial Commit

---
+++ Pennywise/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pennywise
+//
+//  Created by Arnav Varyani on 4/8/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PennywiseTests/PennywiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  PennywiseTests.swift
+//  PennywiseTests
+//
+//  Created by Arnav Varyani on 4/8/25.
+//
+
+import Testing
+@testable import Pennywise
+
+struct PennywiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
